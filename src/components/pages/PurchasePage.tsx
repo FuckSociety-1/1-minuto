d5a9215cc2d53bcdc364b496bdb05111d7a0c0cd
Wix Remote Machine <support@wix.com>
2025-12-28 02:19:04 +0000

    Manual code changes

--- src/components/pages/PurchasePage.tsx
+++ src/components/pages/PurchasePage.tsx
@@ -124,7 +124,7 @@ export default function PurchasePage() {
               </div>
 
               {/* File Upload */}
-              <div className="space-y-2">
+              <div className="space-y-12">
                 <Label htmlFor="content" className="font-paragraph text-base text-soft-white">
                   Subir Imagen o Video
                 </Label>
